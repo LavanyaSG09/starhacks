86730cff99067f11d05cd5c75d0437af6fd57010
Lavanya S.G <106330766+LavanyaSG09@users.noreply.github.com>
2023-07-21 11:22:13 +0530

    Add files via upload

---
+++ createb.php
@@ -0,0 +1,27 @@
+<?php
+include "config.php";
+if (isset($_POST['SUBMIT'])) {
+    // get variables from the form
+    $state = $_POST['statename'];
+    $district = $_POST['distname'];
+    $blood_group = $_POST['bloodtype'];
+    $whole_blood = $_POST['wholename'];
+
+
+    //write sql queryb
+
+    $sql = "INSERT INTO `bloodstk`(`statename`, `distname`, `bloodtype`, `wholename`) VALUES ('$state','$district','$blood_group','$whole_blood')";
+    // execute the query
+
+    $result = $conn->query($sql);
+
+    if ($result == TRUE) {
+        echo "New record created successfully.";
+    }
+    else
+    {
+        echo "Error:". $sql . "<br>". $conn->error;
+    }
+
+    $conn->close();
+?>
